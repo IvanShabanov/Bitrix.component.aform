e407dbe1f757cba244ce2d5dae9495dcfc70f55b
Иван Шабанов <coder10@web-axioma.ru>
2021-10-04 15:18:43 +0700

    Шаблон Default

--- components/is_pro/aform/templates/.default/script.js
+++ components/is_pro/aform/templates/.default/script.js
@@ -1,84 +1,70 @@
-function Check_this_value_empty(el) {
-    if (el.value == '') {
-        $(el).closest('.field').removeClass('active');
-    } else {
-        $(el).closest('.field').addClass('active');
-    }
+function Default_CheckForm() {
+    let content_div = document.querySelectorAll('.FRONTEND_FORM');
+    if (content_div.length > 0) {
+        content_div.forEach(function (form, index) {
+            let this_div = form;
+            let this_div_id = form.getAttribute('id');
+            console.log(this_div_id);
+            if (!form.classList.contains('inited')) {
+                form.classList.add('inited');
+                form.addEventListener('submit', function (event) {
+                    event.preventDefault();
+                    let action_url = form.getAttribute('action');
+                    if ((action_url == null) || (typeof action_url == 'undefined')) {
+                        action_url = window.location;
+                    }
+                    let data = new FormData();
+
+                    const fields = form.querySelectorAll('input, select, textarea');
+                    fields.forEach(function (field) {
+                        let input_type = field.getAttribute('type');
+                        let input_name = field.getAttribute('name');
+                        if (input_type == 'file') {
+                            for (let i = 0; i < field.files.length; i++) {
+                                data.append(input_name, field.files[i]);
+                            };
+                        } else if (input_type == 'checkbox') {
+                            if (field.checked) {
+                                data.append(input_name, field.value);
+                            };
+                        } else if (input_type == 'radio') {
+                            if (field.checked) {
+                                data.append(input_name, field.value);
+                            };
+                        } else {
+                            data.append(input_name, field.value);
+                        }
+                    });
+                    fetch(action_url, {
+                        method: 'POST',
+                        body: data,
+                    }).then(function (response) {
+                        return response.text();
+                    }).then(function (html) {
+                        let parser = new DOMParser();
+                        let doc = parser.parseFromString(html, 'text/html');
+                        let msg = doc.querySelector('#' + this_div_id);
+                        console.log(this_div_id);
+                        this_div.innerHTML = msg.innerHTML;
+                    })
+                    .catch(function (err) {
+                        console.log('Something went wrong. ', err);
+                    });
+
+                });
+            };
+        });
+    };
 }
 
 
-function InitAxiForm() {
+document.addEventListener('DOMContentLoaded', function(){
+    /*
     const doc = $('body')[0];
     const MutationObserver = window.MutationObserver;
-    const myObserver = new MutationObserver(CheckAxiForm);
+    const myObserver = new MutationObserver(Default_CheckForm);
     const obsConfig = { childList: true, characterData: true, attributes: true, subtree: true };
     myObserver.observe(doc, obsConfig);
-    CheckAxiForm();
-}
-
-function CheckAxiForm() {
-    var content_div = $('.FRONTEND_FORM');
-    $(content_div).each(function () {
-        var this_div = $(this);
-        var this_div_id = $(this).attr('id');
-        if (!$(this).find('form').hasClass('inited')) {
-            $(this).find('form').addClass('inited');
-
-            $(this).find(' input, textarea').each(function () {
-                Check_this_value_empty(this);
-                $(this).on('keyup', function () {
-                    Check_this_value_empty(this);
-                })
-                $(this).on('focus', function () {
-                    $(this).closest('.field').addClass('active');
-                });
-                $(this).on('focusout', function () {
-                    Check_this_value_empty(this);
-                });
-            });
-
-
-
-            $(this).find('form').submit(function (event) {
-                event.preventDefault();
-                var action_url = $(this).attr('action');
-                var data = new FormData();
-                /* Собираем все поля */
-                $(this).find('input, select, textarea').each(function () {
-                    var input_type = $(this).attr('type');
-                    var input_name = $(this).attr('name');
-                    if (input_type == 'file') {
-                        data.append(input_name, $(this)[0].files[0]);
-                    } else if (input_type == 'checkbox') {
-                        if ($(this).prop('checked')) {
-                            data.append(input_name, $(this).val());
-                        };
-                    } else if (input_type == 'radio') {
-                        if ($(this).prop('checked')) {
-                            data.append(input_name, $(this).val());
-                        };
-                    } else {
-                        data.append(input_name, $(this).val());
-                    }
-                });
-                $.ajax({
-                    type: "POST",
-                    processData: false,
-                    contentType: false,
-                    url: action_url,
-                    data: data,
-                    success: function (data) {
-                        var msg = '';
-                        msg = $(data).find('#' + this_div_id).html();
-                        $(this_div).html(msg);
-                    }
-                });
-            });
-        };
-    });
-}
-
-
-$(document).ready(function () {
-    InitAxiForm();
+    */
+    Default_CheckForm();
 });
